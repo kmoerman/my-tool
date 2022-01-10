5d0f7652647ba6ad6989acd66448293ac2f385ee
Klaas Moerman <klaasmoerman@gmail.com>
2022-01-10 21:18:29 +0100

    extend tool action

--- tool.js
+++ tool.js
@@ -1,7 +1,18 @@
+const fs = require('fs/promises')
+
+const file = process.env.INPUT_FILE
+
 if (process.env.INPUT_DEPENDENCIES === 'true') {
+  // import dummy dependency lodash
   const _ = require('lodash')
-  console.log(`My tool with dependency lodash:${_.VERSION}; processing ${process.env.INPUT_FILE}`)
+  console.log(`My tool with dependency lodash:${_.VERSION}; processing ${file}`)
+
 }
 else {
-  console.log(`My tool without dependency; processing ${process.env.INPUT_FILE}`)
+  console.log(`My tool without dependency; processing ${file}`)
 }
+
+fs.readFile(file)
+  .then( data => console.log(data.toString())
+       , err  => { process.exitCode = 1; console.error(err) }
+       )
